04caee8acfda6e10dbd2897b3cd18236dd8206f2
GoldcrafterXD <13365809+GoldcrafterXD@users.noreply.github.com>
2026-02-26 23:19:00 +0100

    Sharkord Client
    - Removed Dev Overlay

--- main.js
+++ main.js
@@ -297,7 +297,7 @@ function createWindow() {
   mainWindow.loadURL(`file://${__dirname}/index.html`); // Change to your Sharkord URL
 
   // Open DevTools in development
-  mainWindow.webContents.openDevTools();
+  //mainWindow.webContents.openDevTools();
 
   mainWindow.on('closed', () => {
     mainWindow = null;
